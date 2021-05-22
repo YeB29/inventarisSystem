7fc0a664a381d3530eff46eca4354a51d86c0760
YeB29 <74351462+YeB29@users.noreply.github.com>
2021-05-22 17:35:13 +0200

    product classen testing

---
+++ src/Product.java
@@ -0,0 +1,18 @@
+public class Product {
+    private String name;
+    private String productId;
+
+    public Product(String name, String productId){
+        this.name= name;
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+}
+
---
+++ test/ProductTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+class ProductTest {
+
+    @Test
+    void getNameTest() {
+    }
+
+    @Test
+    void getProductIdTest() {
+    }
+}
